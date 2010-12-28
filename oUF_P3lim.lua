ab1603b24537d84ff478f2f4427835fb9da993ee
p3lim <adrianlund@gmail.com>
2010-12-28 06:10:49 +0100

    Issues with RegisterAttributeDriver seems to be fixed

--- oUF_P3lim.lua
+++ oUF_P3lim.lua
@@ -164,8 +164,7 @@ local UnitSpecific = {
 local function Shared(self, unit)
 	self.colors.power.MANA = {0, 144/255, 1}
 
-	-- XXX: Change to AnyUp when RegisterAttributeDriver doesn't cause clicks
-	self:RegisterForClicks('AnyDown')
+	self:RegisterForClicks('AnyUp')
 	self:SetScript('OnEnter', UnitFrame_OnEnter)
 	self:SetScript('OnLeave', UnitFrame_OnLeave)
 
