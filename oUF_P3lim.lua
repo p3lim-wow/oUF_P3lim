0b631bf957c7244fdb03264fd6b0d6ca494d6541
Adrian L Lange <mail@p3lim.net>
2016-05-20 19:10:36 +0200

    Change the color for insanity

--- oUF_P3lim.lua
+++ oUF_P3lim.lua
@@ -418,6 +418,7 @@ local function Shared(self, unit)
 	unit = unit:match('(boss)%d?$') or unit:match('(arena)%d?$') or unit
 
 	self.colors.power.MANA = {0, 144/255, 1}
+	self.colors.power.INSANITY = {4/5, 2/5, 1}
 
 	self:RegisterForClicks('AnyUp')
 	self:SetScript('OnEnter', UnitFrame_OnEnter)
